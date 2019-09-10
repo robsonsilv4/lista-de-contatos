ae1294fd764541bacdd29d5e1ed85057ea4ca313
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-10 20:56:09 -0300

    Projeto inicial

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Projeto Inicial</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
